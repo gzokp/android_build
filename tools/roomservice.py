21f25bf3a9e8f4ac3e53c4f1dfc0db2f7967a971
somber73 <nytesblood@gmail.com>
2014-03-22 15:40:30 -0400

    roomservice edits

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -34,12 +34,12 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "cr"
+default_rem = "gz"
 # set this to the default revision to use (branch/tag name)
-default_rev = "kk"
+default_rev = "gzokp"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
-default_team_rem = "cr"
+default_team_rem = "gh"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
@@ -206,7 +206,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "carbon.dependencies"
+    dep_file = "gzokp.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
